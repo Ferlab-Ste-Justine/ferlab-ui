c0cda094baa3b7720a2a11c175ac3a4c4942ee66
Francis Lavoie <flavoie.crsj+github@gmail.com>
2020-10-08 16:26:00 -0400

    remove background default color for layout/grid

--- packages/core-react/src/layout/Grid.tsx
+++ packages/core-react/src/layout/Grid.tsx
@@ -1,13 +1,17 @@
 import React, { FunctionComponent } from 'react';
 
 type Props = {
+    backgroundColor?: string,
     children: React.ReactNode;
+    className: string
 };
 
-const Grid: FunctionComponent<Props> = ({ children }) => (
-    <div className="core-layout-grid--content core-layout-grid--content-summary">
+const Grid: FunctionComponent<Props> = ({ backgroundColor, children, className }) => {
+    const styles: React.CSSProperties = {};
+    if (backgroundColor) styles.backgroundColor = backgroundColor;
+    return <div style={{...styles}} className={`core-layout-grid--content core-layout-grid--content-summary ${className}`}>
         {children}
     </div>
-)
+}
 
 export default Grid;
