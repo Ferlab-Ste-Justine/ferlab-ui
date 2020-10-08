2e7681eff7862ecf1c34aea169d93dcd8fc9f5af
Francis Lavoie <flavoie.crsj+github@gmail.com>
2020-10-07 23:09:53 -0400

    update core-react structure

--- packages/core-react/src/cards/GridCard.tsx
+++ packages/core-react/src/cards/GridCard.tsx
@@ -1,6 +1,5 @@
 import React, { ReactElement, useRef, RefObject } from 'react';
-import { Card as AntdCard } from 'antd';
-import './Card.scss';
+import { Card } from 'antd';
 
 type CardProps = {
   title?: string;
@@ -8,21 +7,21 @@ type CardProps = {
   children: ReactElement[] | React.ReactNode | Function;
 };
 
-const Card = ({ title, loading, children, ...props }: CardProps) => {
+const GridCard = ({ title, loading, children, ...props }: CardProps) => {
   const divElem = useRef() as RefObject<HTMLDivElement> | undefined;
   return (
-    <AntdCard title={title} loading={loading} className="cb-card-summary-chart" {...props}>
-      <div className="summary-card-content-container" ref={divElem}>
+    <Card title={title} loading={loading} className="core-grid-card" {...props}>
+      <div className="core-grid-card-content-container" ref={divElem}>
         {typeof children === 'function' ? (
-          <div className="dynamic-content">
+          <div className="core-grid-card-dynamic-content">
             {children({ height: divElem?.current?.offsetHeight })}
           </div>
         ) : (
           children
         )}
       </div>
-    </AntdCard>
+    </Card>
   );
 };
 
-export default Card;
+export default GridCard;
