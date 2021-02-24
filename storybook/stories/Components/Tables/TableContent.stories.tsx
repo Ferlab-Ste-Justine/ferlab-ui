4a47beeb8f808990d7bd8f6d9db4afc41e69161c
Adrian Paul <adipaul1981@yahoo.ca>
2021-02-24 09:23:51 -0500

    fix storybook for tables content

--- storybook/stories/Components/Tables/TableContent.stories.tsx
+++ storybook/stories/Components/Tables/TableContent.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Meta, Story} from '@storybook/react/types-6-0';
 import TableContent, {TableContentProps} from "@ferlab/ui/components/tables/TableContent";
-import {Table} from "antd";
+import {Button, Table} from "antd";
 
 
 export default {
@@ -25,52 +25,26 @@ const TableContentStory = ({title, style, ...props} : {title: string, style: Rea
   </>
 );
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name'
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-];
-
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-  },
-];
-
-/* HORIZONTAL */
 export const StackLayoutHorizontal = TableContentStory.bind({});
+const arrayLength = Array.from(Array(20).keys())
 StackLayoutHorizontal.args = {
-  title: 'Horizontal',
+  title: 'TableContent',
   children: (
-      // <Table columns={columns} dataSource={data} />
-      <div>totototo</div>
+      <table>
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+          {
+              arrayLength.map(x =>
+                  <tr>
+                      <td>{`Alfreds Futterkiste ${x}`}</td>
+                      <td>{`Maria Anders ${x}`}</td>
+                      <td>{`Germany ${x}`}</td>
+                  </tr>)
+          }
+      </table>
   ),
 };
 
