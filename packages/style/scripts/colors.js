6fbd3901d51cbee50474e624e18ecf5fd9db0b48
Kevin Jacques <kjacques@ferlab.bio>
2021-04-28 09:23:53 -0400

    feat: Add new colors + fix colors compilation

--- packages/style/scripts/colors.js
+++ packages/style/scripts/colors.js
@@ -2,6 +2,9 @@ const spawn = require('cross-spawn');
 const path = require('path');
 
 // Once we have more themes, loop through all of them to generate the colors from antd
+spawn.sync('rm', [`./themes/cqdg/antd/_colors.scss`], {
+    stdio: 'inherit'
+}); 
 spawn.sync('less2sass', [`./themes/cqdg/antd/_colors.less`], {
     stdio: 'inherit'
 });
