8b109777bd4251588f0a57a7d1036286d32c7bdf
Kevin Jacques <kjacques@ferlab.bio>
2021-05-28 13:29:16 -0400

    feat(CQDG): #366 refactor CountWithIcon to MultiLabel and support Top Icon placement

---
+++ packages/ui/src/components/labels/MultiLabel.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import '@ferlab/style/components/labels/MultiLabel.scss';
+
+export enum MultiLabelTypeEnum {
+    Inline = 'inline',
+    Stack = 'stack',
+}
+export enum MultiLabelIconPositionEnum {
+    Middle = 'middle-icon',
+    Top = 'top-icon',
+}
+
+export type MultiLabelProps = {
+    className?: string;
+    labelClassName?: string;
+    subLabelClassName?: string;
+    iconPosition?: MultiLabelIconPositionEnum;
+    Icon: React.ReactNode;
+    type?: MultiLabelTypeEnum;
+    label: React.ReactNode;
+    subLabel: React.ReactNode;
+    style?: React.CSSProperties;
+};
+
+const MultiLabel: React.FC<MultiLabelProps> = ({
+    className = '',
+    labelClassName = '',
+    subLabelClassName = '',
+    Icon,
+    label,
+    subLabel,
+    type = MultiLabelTypeEnum.Stack,
+    iconPosition = MultiLabelIconPositionEnum.Middle,
+    style = {},
+}) => (
+    <div
+        className={`multilabel__container multilabel__container--${type} multilabel__container--${iconPosition} ${className}`}
+        style={style}
+    >
+        <div className="wrapper__icon">{Icon}</div>
+        <span className={`wrapper__label ${labelClassName}`}>{label}</span>
+        <span className={`wrapper__subLabel ${subLabelClassName}`}>{subLabel}</span>
+    </div>
+);
+
+export default MultiLabel;
