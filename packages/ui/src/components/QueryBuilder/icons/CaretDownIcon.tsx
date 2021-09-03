3ee0975e8c593dbf2236d7823fe300f24c02a576
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2021-09-03 14:37:50 -0400

    Small refactor

---
+++ packages/ui/src/components/QueryBuilder/icons/CaretDownIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import styles from '@ferlab/style/components/queryBuilder/LabelOperator.module.scss';
+
+interface IElementlOperatorProps {
+    className?: string;
+}
+const CaretDownIcon = ({ className = '' }: IElementlOperatorProps) => (
+    <svg
+        className={`${styles.icon} ${className}`}
+        fill="currentColor"
+        height="14"
+        viewBox="0 0 24 24"
+        width="14"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M19.6962 7.03137H4.30248C3.84077 7.03137 3.58295 7.51887 3.86889 7.85168L11.5658 16.7767C11.7861 17.0322 12.2103 17.0322 12.433 16.7767L20.1298 7.85168C20.4158 7.51887 20.158 7.03137 19.6962 7.03137Z" />
+    </svg>
+);
+export default CaretDownIcon;
---
+++ packages/ui/src/components/utils/ConditionalWrapper.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react';
+
+interface IConditionalWrapperProps {
+    condition: boolean;
+    wrapper: (children: JSX.Element) => JSX.Element;
+    children: JSX.Element;
+}
+
+export default ({ condition, wrapper, children }: IConditionalWrapperProps) =>
+    condition ? wrapper(children) : children;
