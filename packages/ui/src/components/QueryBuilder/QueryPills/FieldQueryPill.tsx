0c54bbdc23c1279b67081d9ac35702b514382275
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2021-09-29 13:17:55 -0400

    Small fixes

--- packages/ui/src/components/QueryBuilder/QueryPills/FieldQueryPill.tsx
+++ packages/ui/src/components/QueryBuilder/QueryPills/FieldQueryPill.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, MouseEvent } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import { Button, Dropdown, Spin } from 'antd';
 import cx from 'classnames';
