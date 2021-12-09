eb894b5275006893bb1115025d41445e20c68246
Francis Lavoie <flavoie@ferlab.bio>
2021-12-09 09:42:49 -0500

    feat: #clin-548 new label for Status

---
+++ storybook/stories/Components/Labels/Status.stories.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Meta } from "@storybook/react/types-6-0";
+import StatusLabel, {
+    StatusOptions as Status,
+    StatusLabelProps,
+} from "@ferlab/ui/components/labels/Status";
+
+export default {
+    title: "@ferlab/Components/Labels/Status",
+    component: StatusLabel,
+    decorators: [
+        (Story) => (
+            <>
+                <h2>{Story}</h2>
+                <Story />
+            </>
+        ),
+    ],
+    argTypes: {
+        className: {
+            control: "string",
+        },
+        dictionary: {
+            control: {
+                type: "object",
+            },
+        },
+    },
+} as Meta;
+
+const translationDictionary = {
+    [Status.Active]: "Approved",
+    [Status.Completed]: "Completed",
+    [Status.Draft]: "Draft",
+    [Status.Revoked]: "Refused",
+    [Status.Submitted]: "Submitted",
+    [Status.Incomplete]: "Incomplete",
+};
+
+const StatusLabelPropsStory = ({
+    style,
+    ...props
+}: {
+    title: string;
+    style: React.CSSProperties;
+    props: StatusLabelProps;
+}) => (
+    <>
+        {Object.values(Status).map((s) => (
+            <>
+                <StatusLabel
+                    dictionary={translationDictionary}
+                    status={s}
+                    {...props}
+                />
+                <br />
+                <br />
+            </>
+        ))}
+    </>
+);
+
+export const StatusLabelPropsInComplete = StatusLabelPropsStory.bind({});
+StatusLabelPropsInComplete.args = {
+    dictionary: translationDictionary,
+};
