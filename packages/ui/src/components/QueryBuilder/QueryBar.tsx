df24f345b0911175035f22161fcd2f9ef6792d30
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2021-12-16 16:32:57 -0500

    Fix query bar

--- packages/ui/src/components/QueryBuilder/QueryBar.tsx
+++ packages/ui/src/components/QueryBuilder/QueryBar.tsx
@@ -5,7 +5,13 @@ import cx from 'classnames';
 
 import StackLayout from '../../layout/StackLayout';
 import BooleanQueryPill from './QueryPills/BooleanQueryPill';
-import { IDictionary, IFacetFilterConfig, TCallbackRemoveAction, TCallbackRemoveReferenceAction, TOnChange } from './types';
+import {
+    IDictionary,
+    IFacetFilterConfig,
+    TCallbackRemoveAction,
+    TCallbackRemoveReferenceAction,
+    TOnChange,
+} from './types';
 import { ISyntheticSqon, TSqonGroupOp } from '../../data/sqon/types';
 import { isBooleanOperator, isEmptySqon } from '../../data/sqon/utils';
 import { numberFormat } from '../../utils/numberUtils';
@@ -58,7 +64,7 @@ const QueryBar = ({
     onCombineChange,
     onSelectBar,
     getColorForReference,
-    facetFilterConfig
+    facetFilterConfig,
 }: IQueryBarProps) => {
     const [checked, setChecked] = useState(isSelected);
     useEffect(() => {
@@ -68,98 +74,103 @@ const QueryBar = ({
     const containerClassNames = cx(styles.queryBarContainer, { [styles.selected]: isActive });
 
     return (
-        <StackLayout
-            className={containerClassNames}
-            fitContent
-            flexContent
-            onClick={() => {
-                if (!isActive) {
-                    onChangeQuery(id, query);
-                }
-            }}
-        >
-            <div className={styles.identifier} style={isReferenced ? { background: referenceColor! } : {}} />
-            {!selectionDisabled && (
-                <StackLayout className={styles.selectionWrapper}>
-                    <Checkbox
-                        checked={checked}
-                        className={styles.label}
-                        onClick={() => {
-                            setChecked(!checked);
-                            onSelectBar(index, checked);
-                        }}
-                    >
-                        {`Q${index + 1}`}
-                    </Checkbox>
-                </StackLayout>
-            )}
-            <StackLayout className={styles.queryContent} flexContent>
-                {isEmptySqon(query) ? (
-                    <StackLayout>{dictionary.query?.noQuery || 'Use the filters to build a query'}</StackLayout>
-                ) : (
-                    isBooleanOperator(query) && (
-                        <StackLayout className={styles.queryValues} fitContent flexContent>
-                            <BooleanQueryPill
-                                parentQueryId={id}
-                                query={query}
-                                isActive={isActive}
-                                dictionary={dictionary}
-                                showLabels={showLabels}
-                                onRemoveFacet={onRemoveFacet}
-                                onRemoveReference={onRemoveReference}
-                                onCombineChange={onCombineChange}
-                                getColorForReference={getColorForReference}
-                                facetFilterConfig={facetFilterConfig}
-                            />
-                        </StackLayout>
-                    )
+        <StackLayout fitContent flexContent>
+            <div
+                className={cx(styles.identifier, isActive ? styles.active : '')}
+                style={isReferenced ? { background: referenceColor! } : {}}
+            />
+            <StackLayout
+                className={containerClassNames}
+                fitContent
+                flexContent
+                onClick={() => {
+                    if (!isActive) {
+                        onChangeQuery(id, query);
+                    }
+                }}
+            >
+                {!selectionDisabled && (
+                    <StackLayout className={styles.selectionWrapper}>
+                        <Checkbox
+                            checked={checked}
+                            className={styles.label}
+                            onClick={() => {
+                                setChecked(!checked);
+                                onSelectBar(index, checked);
+                            }}
+                        >
+                            {`Q${index + 1}`}
+                        </Checkbox>
+                    </StackLayout>
                 )}
-                <span className={styles.total}>
-                    {Icon} {numberFormat(total)}
-                </span>
-            </StackLayout>
-            {!actionDisabled && (
-                <StackLayout className={styles.actions}>
-                    <Tooltip title={dictionary.actions?.duplicate || 'Duplicate'}>
-                        <Button
-                            className={styles.actionButton}
-                            onClick={(e: React.MouseEvent) => {
-                                e.stopPropagation();
-                                onDuplicate(id, query);
-                            }}
-                            type="text"
-                        >
-                            <AiOutlineCopy size={18} />
-                        </Button>
-                    </Tooltip>
-
-                    <Tooltip title={dictionary.actions?.delete?.confirm || 'Delete'}>
-                        <Popconfirm
-                            arrowPointAtCenter
-                            cancelText={dictionary.actions?.delete?.cancel || 'Cancel'}
-                            disabled={!canDelete}
-                            okText={dictionary.actions?.delete?.confirm || 'Delete'}
-                            onConfirm={(e) => {
-                                e!.stopPropagation();
-                                onDeleteQuery(id, query);
-                            }}
-                            placement="topRight"
-                            title={dictionary.actions?.delete?.title || 'Delete this query?'}
-                            getPopupContainer={(trigger) => trigger.parentElement!}
-                        >
+                <StackLayout className={styles.queryContent} flexContent>
+                    {isEmptySqon(query) ? (
+                        <StackLayout>{dictionary.query?.noQuery || 'Use the filters to build a query'}</StackLayout>
+                    ) : (
+                        isBooleanOperator(query) && (
+                            <StackLayout className={styles.queryValues} fitContent flexContent>
+                                <BooleanQueryPill
+                                    parentQueryId={id}
+                                    query={query}
+                                    isActive={isActive}
+                                    dictionary={dictionary}
+                                    showLabels={showLabels}
+                                    onRemoveFacet={onRemoveFacet}
+                                    onRemoveReference={onRemoveReference}
+                                    onCombineChange={onCombineChange}
+                                    getColorForReference={getColorForReference}
+                                    facetFilterConfig={facetFilterConfig}
+                                />
+                            </StackLayout>
+                        )
+                    )}
+                    <span className={styles.total}>
+                        {Icon} {numberFormat(total)}
+                    </span>
+                </StackLayout>
+                {!actionDisabled && (
+                    <StackLayout className={styles.actions}>
+                        <Tooltip title={dictionary.actions?.duplicate || 'Duplicate'}>
                             <Button
                                 className={styles.actionButton}
                                 onClick={(e: React.MouseEvent) => {
                                     e.stopPropagation();
+                                    onDuplicate(id, query);
                                 }}
                                 type="text"
                             >
-                                <AiOutlineDelete size={18} />
+                                <AiOutlineCopy size={18} />
                             </Button>
-                        </Popconfirm>
-                    </Tooltip>
-                </StackLayout>
-            )}
+                        </Tooltip>
+
+                        <Tooltip title={dictionary.actions?.delete?.confirm || 'Delete'}>
+                            <Popconfirm
+                                arrowPointAtCenter
+                                cancelText={dictionary.actions?.delete?.cancel || 'Cancel'}
+                                disabled={!canDelete}
+                                okText={dictionary.actions?.delete?.confirm || 'Delete'}
+                                onConfirm={(e) => {
+                                    e!.stopPropagation();
+                                    onDeleteQuery(id, query);
+                                }}
+                                placement="topRight"
+                                title={dictionary.actions?.delete?.title || 'Delete this query?'}
+                                getPopupContainer={(trigger) => trigger.parentElement!}
+                            >
+                                <Button
+                                    className={styles.actionButton}
+                                    onClick={(e: React.MouseEvent) => {
+                                        e.stopPropagation();
+                                    }}
+                                    type="text"
+                                >
+                                    <AiOutlineDelete size={18} />
+                                </Button>
+                            </Popconfirm>
+                        </Tooltip>
+                    </StackLayout>
+                )}
+            </StackLayout>
         </StackLayout>
     );
 };
