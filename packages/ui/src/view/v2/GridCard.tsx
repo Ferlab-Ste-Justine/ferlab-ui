e3065470de78341f3515c6bea437eed9f7aeb968
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2021-12-17 14:31:30 -0500

    SJIP-96: Add new card component

---
+++ packages/ui/src/view/v2/GridCard.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Card, { CardProps } from 'antd/lib/card';
+import cx from 'classnames';
+import ConditionalWrapper from '../../components/utils/ConditionalWrapper';
+
+import styles from '@ferlab/style/view/cards/v2/gridcard.module.scss';
+import { Spin } from 'antd';
+
+type OwnProps = CardProps & {
+    footer?: React.ReactNode;
+    content: React.ReactNode;
+    theme?: 'shade' | 'light';
+    loadingType?: 'skeleton' | 'spinner';
+};
+
+const getWrapperClass = (footer: React.ReactNode, title: React.ReactNode) =>
+    !footer && !title ? styles.contentOnly : !footer ? styles.withHeaderOnly : !title ? styles.withFooterOnly : '';
+
+const GridCard = ({
+    footer,
+    content,
+    theme = 'light',
+    loadingType = 'skeleton',
+    ...rest
+}: Omit<OwnProps, 'actions'>) => {
+    return (
+        <Card
+            {...rest}
+            loading={loadingType === 'skeleton' ? rest.loading : false}
+            actions={footer ? [footer] : undefined}
+            className={cx(
+                styles.fbUIGridCard,
+                theme == 'light' ? styles.light : styles.shade,
+                getWrapperClass(footer, rest.title),
+            )}
+        >
+            <ConditionalWrapper
+                condition={loadingType === 'spinner'}
+                wrapper={(children) => <Spin spinning={rest.loading}>{children}</Spin>}
+                children={<>{content}</>}
+            />
+        </Card>
+    );
+};
+
+export default GridCard;
