e2c438ad1c85e62a34c20d8a27679cfe6fad157e
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2022-03-16 10:47:32 -0400

    Fix key enhance

--- packages/ui/src/data/arranger/formatting.ts
+++ packages/ui/src/data/arranger/formatting.ts
@@ -3,34 +3,34 @@ export const ArrangerValues = {
     noData: 'No Data',
     true: '1',
     false: '0',
-}
+};
 
-export const AggregateValues= {
+export const AggregateValues = {
     true: 'true',
-    false: 'false'
-}
+    false: 'false',
+};
 export const keyEnhance = (key: string, s: string = ArrangerValues.noData): string => {
-  switch (key) {
-    case ArrangerValues.missing:
-      return s;
-    case ArrangerValues.true:
-      return AggregateValues.true;
-    case ArrangerValues.false:
-      return AggregateValues.false;
-    default:
-      return key;
-  }
+    switch (key) {
+        case ArrangerValues.missing:
+            return s;
+        case ArrangerValues.true:
+            return AggregateValues.true;
+        case ArrangerValues.false:
+            return AggregateValues.false;
+        default:
+            return key;
+    }
 };
 
 export const keyEnhanceBooleanOnly = (key: string) => {
-  switch (key) {
-    case ArrangerValues.true:
-      return AggregateValues.true;
-    case ArrangerValues.true:
-      return AggregateValues.false;
-    default:
-      return key;
-  }
+    switch (key) {
+        case ArrangerValues.true:
+            return AggregateValues.true;
+        case ArrangerValues.false:
+            return AggregateValues.false;
+        default:
+            return key;
+    }
 };
 
 export const dotToUnderscore = (str: string) => str.replaceAll('.', '__');
