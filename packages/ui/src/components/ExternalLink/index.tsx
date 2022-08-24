f1bc4e0cc2129491a9c2f34d7ac02aa844b9bc46
claudia1296 <claudia.roy.cr@gmail.com>
2022-08-24 12:01:50 -0400

    fix: CLIN-1367 externalLink and operator

--- packages/ui/src/components/ExternalLink/index.tsx
+++ packages/ui/src/components/ExternalLink/index.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import { Space } from 'antd';
 import cx from 'classnames';
+
+import ExternalLinkIcon from './ExternalLinkIcon';
 
 import styles from '@ferlab/style/components/prolabel/ProLabel.module.scss';
 
 export type IExternalLinkProps = Omit<
     React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & {
         children: React.ReactNode;
+        hasIcon?: boolean;
     },
     'rel' | 'target'
 >;
 
 const ExternalLink = (props: IExternalLinkProps) => (
     <a {...props} className={cx(styles.fuiExternalLink, props.className)} rel="noreferrer" target="_blank">
-        {props.children}
+        <Space size={5.75}>
+            {props.children}
+
+            {props.hasIcon === true ? <ExternalLinkIcon height="14" width="14" /> : null}
+        </Space>
     </a>
 );
 
