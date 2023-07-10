beaaf69cf855494ba76c0e14763e39abc1435862
Gaelle Altefrohne <galtefrohne@ferlab.bio>
2023-07-10 14:35:36 -0400

    feat(custom pill): CLIN-2049 add pill sidebar

---
+++ packages/ui/src/components/CustomPill/QueriesSidebar/index.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import {
+    BarChartOutlined,
+    DotChartOutlined,
+    FundOutlined,
+    InfoCircleOutlined,
+    PieChartOutlined,
+} from '@ant-design/icons';
+import { Tooltip, Typography } from 'antd';
+
+import ExternalLink from '../../ExternalLink';
+import { ISavedFilter } from '../../QueryBuilder/types';
+
+import QueryPill from './QueryPill';
+
+import styles from './index.module.scss';
+
+interface IQueriesSidebarDictionary {
+    emptyText: string;
+    learnMore: string;
+    title: string;
+}
+
+interface IQueriesSidebar {
+    customPills: ISavedFilter[];
+    dictionary: IQueriesSidebarDictionary;
+}
+
+const QueriesSidebar = ({ customPills = [], dictionary }: IQueriesSidebar): JSX.Element => {
+    if (!customPills.length) {
+        return (
+            <div className={styles.emptyQueriesSidebarWrapper}>
+                <div className={styles.iconsWrapper}>
+                    <BarChartOutlined className={styles.icon} />
+                    <PieChartOutlined className={styles.icon} />
+                    <FundOutlined className={styles.icon} />
+                    <DotChartOutlined className={styles.icon} />
+                </div>
+                <div className={styles.emptyText}>{dictionary.emptyText}</div>
+
+                {/* TODO change href when we will have it */}
+                <ExternalLink className={styles.link} hasIcon={true} href={'http://www.google.fr'}>
+                    {dictionary.learnMore}
+                </ExternalLink>
+            </div>
+        );
+    }
+
+    const sortedPills = customPills.sort((a, b) => (a.title < b.title ? -1 : a.title > b.title ? 1 : 0));
+
+    return (
+        <div className={styles.queriesSidebarWrapper}>
+            <span className={styles.titleWrapper}>
+                <Typography.Text className={styles.title} strong>
+                    {dictionary.title}
+                </Typography.Text>
+                {dictionary.emptyText && (
+                    <Tooltip
+                        align={{ offset: [-24, 0] }}
+                        arrowPointAtCenter
+                        placement="topLeft"
+                        title={dictionary.emptyText}
+                    >
+                        <InfoCircleOutlined className={styles.tooltipIcon} />
+                    </Tooltip>
+                )}
+            </span>
+            <div className={styles.pillsWrapper}>
+                {sortedPills.map((pill) => (
+                    <QueryPill key={pill.id} queryPill={pill} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default QueriesSidebar;
