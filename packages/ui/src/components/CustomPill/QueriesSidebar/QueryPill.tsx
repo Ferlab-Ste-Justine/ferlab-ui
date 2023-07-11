03cf1e6824ed691d3436eb66cb459413dc148fed
Gaelle Altefrohne <galtefrohne@ferlab.bio>
2023-07-11 12:07:02 -0400

    feat(custom pill): CLIN-2049 extract action method

--- packages/ui/src/components/CustomPill/QueriesSidebar/QueryPill.tsx
+++ packages/ui/src/components/CustomPill/QueriesSidebar/QueryPill.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { CopyOutlined, DeleteOutlined, EditOutlined } from '@ant-design/icons';
-import { Button, Space, Typography } from 'antd';
+import { Button, Typography } from 'antd';
 
 import { ISavedFilter } from '../../QueryBuilder/types';
 
@@ -8,48 +8,38 @@ import styles from './index.module.scss';
 
 interface IQueryPill {
     queryPill: ISavedFilter;
+    addPillToQuery: (queryPillId: string) => void;
+    editPill: (queryPillId: string) => void;
+    duplicatePill: (queryPillId: string) => void;
+    deletePill: (queryPillId: string) => void;
 }
 
-const QueryPill = ({ queryPill }: IQueryPill): JSX.Element => {
-    const addPillToQuery = (pillId: string) => {
-        console.log('add pill to query', pillId);
-    };
-    const editPill = (pillId: string) => {
-        console.log('edit pill');
-    };
-    const duplicatePill = (pillId: string) => {
-        console.log('duplicate pill');
-    };
-    const deletePill = (pillId: string) => {
-        console.log('delete pill');
-    };
-    return (
-        <div className={styles.queryPillWrapper}>
-            <Button className={styles.queryPill} onClick={() => addPillToQuery(queryPill.id)}>
-                <Typography.Text ellipsis={{ tooltip: queryPill.title }}>{queryPill.title}</Typography.Text>
-            </Button>
-            <div className={styles.queryPillEditionWrapper}>
-                <Button
-                    className={styles.queryPillEditionButton}
-                    icon={<EditOutlined />}
-                    onClick={() => editPill(queryPill.id)}
-                    size="small"
-                />
-                <Button
-                    className={styles.queryPillEditionButton}
-                    icon={<CopyOutlined />}
-                    onClick={() => duplicatePill(queryPill.id)}
-                    size="small"
-                />
-                <Button
-                    className={styles.queryPillEditionButton}
-                    icon={<DeleteOutlined />}
-                    onClick={() => deletePill(queryPill.id)}
-                    size="small"
-                />
-            </div>
+const QueryPill = ({ addPillToQuery, deletePill, duplicatePill, editPill, queryPill }: IQueryPill): JSX.Element => (
+    <div className={styles.queryPillWrapper}>
+        <Button className={styles.queryPill} onClick={() => addPillToQuery(queryPill.id)}>
+            <Typography.Text ellipsis={{ tooltip: queryPill.title }}>{queryPill.title}</Typography.Text>
+        </Button>
+        <div className={styles.queryPillEditionWrapper}>
+            <Button
+                className={styles.queryPillEditionButton}
+                icon={<EditOutlined />}
+                onClick={() => editPill(queryPill.id)}
+                size="small"
+            />
+            <Button
+                className={styles.queryPillEditionButton}
+                icon={<CopyOutlined />}
+                onClick={() => duplicatePill(queryPill.id)}
+                size="small"
+            />
+            <Button
+                className={styles.queryPillEditionButton}
+                icon={<DeleteOutlined />}
+                onClick={() => deletePill(queryPill.id)}
+                size="small"
+            />
         </div>
-    );
-};
+    </div>
+);
 
 export default QueryPill;
