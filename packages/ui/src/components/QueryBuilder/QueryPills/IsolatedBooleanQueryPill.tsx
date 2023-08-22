dfedb367b0b40badbdc630640f511c05950a7f96
ethienneroy <ethienner@gmail.com>
2023-08-22 14:01:14 -0400

    fix: CLIN-1913 exploded term filters with noData + changed some-not-in

--- packages/ui/src/components/QueryBuilder/QueryPills/IsolatedBooleanQueryPill.tsx
+++ packages/ui/src/components/QueryBuilder/QueryPills/IsolatedBooleanQueryPill.tsx
@@ -1,6 +1,9 @@
-import { cloneDeep } from 'lodash';
 import React from 'react';
+import { cloneDeep } from 'lodash';
+
+import { BooleanOperators, TermOperators } from '../../../data/sqon/operators';
 import { ISqonGroupFilter, IValueFilter } from '../../../data/sqon/types';
+
 import FieldQueryPill from './FieldQueryPill';
 
 interface IIsolatedBooleanQueryPillProps {
@@ -9,15 +12,22 @@ interface IIsolatedBooleanQueryPillProps {
     contentValue: ISqonGroupFilter;
 }
 
-const IsolatedBooleanQueryPill = ({ contentValue, onRemove, isBarActive }: IIsolatedBooleanQueryPillProps) => {
+const IsolatedBooleanQueryPill = ({ contentValue, isBarActive, onRemove }: IIsolatedBooleanQueryPillProps) => {
     const newQueryDict: { [content: string]: any } = {};
-    
+
     contentValue.content.forEach((f: any) => {
         if (newQueryDict.hasOwnProperty(f.content.field)) {
             newQueryDict[f.content.field] = cloneDeep(newQueryDict[f.content.field]);
             newQueryDict[f.content.field].content.value = newQueryDict[f.content.field].content.value.concat(
                 f.content.value,
             );
+            if (
+                contentValue.skipBooleanOperatorCheck &&
+                f.op === TermOperators.in &&
+                contentValue.op === BooleanOperators.and
+            ) {
+                newQueryDict[f.content.field].op = TermOperators.all;
+            }
         } else {
             newQueryDict[f.content.field] = cloneDeep(f);
         }
