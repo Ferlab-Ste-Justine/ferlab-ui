feecdc8b1cf693ca0cb5e4a0a346e5319f1f434a
lflangis <lucf.langis@gmail.com>
2024-08-12 15:56:32 -0400

    feat(description): add basic-bordered theme for variant entity summary

--- storybook/stories/Pages/EntityPage/EntityVariantSummary.stories.tsx
+++ storybook/stories/Pages/EntityPage/EntityVariantSummary.stories.tsx
@@ -72,3 +72,56 @@ BasicEntityVariantSummary.args = {
         },
     },
 };
+
+
+export const BorderedEntityVariantSummary = EntityVariantSummaryStory.bind({});
+BorderedEntityVariantSummary.args = {
+    id: 'ID',
+    loading: false,
+    noDataLabel: 'No data available',
+    data: {
+        banner: [
+            { label: 'Item 1', value: 'Value 1' },
+            { label: 'Item 2', value: 'Value 2' },
+            { label: 'Item 3', value: 'Value 3' },
+            { label: 'Item 4', value: 'Value 4' },
+            { label: 'Item 5', value: 'Value 5' },
+        ],
+        info: ['Item 1', 'Item 2', 'Item 3', 'Item 4'],
+        details: {
+            leftSection: {
+                title: 'Block 1',
+                items: [
+                    { label: 'Item 1', value: 'Value 1' },
+                    { label: 'Item 2', value: 'Value 2' },
+                    { label: 'Item 3', value: 'Value 3' },
+                    { label: 'Item 4', value: 'Value 4' },
+                    { label: 'Item 5', value: 'Value 5' },
+                    { label: 'Item 6', value: 'Value 6' },
+                ],
+            },
+            middleSection: [
+                {
+                    title: 'Block 2.1',
+                    items: [
+                        { label: 'Item 1', value: 'Value 1' },
+                        { label: 'Item 2', value: 'Value 2' },
+                    ],
+                },
+                {
+                    title: 'Block 2.2',
+                    items: [{ label: 'Item 1', value: 'Value 1' }],
+                },
+            ],
+            rightSection: {
+                title: 'Block 3',
+                items: [
+                    { label: 'Item 1', value: 'Value 1' },
+                    { label: 'Item 2', value: 'Value 2' },
+                    { label: 'Item 3', value: 'Value 3' },
+                ],
+            },
+        },
+    },
+    theme: 'basic-bordered'
+};
