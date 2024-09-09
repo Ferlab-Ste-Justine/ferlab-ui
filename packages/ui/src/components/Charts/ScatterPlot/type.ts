d9fda29e61ebc8e97062f92af79074e685e2ad1a
lflangis <lucf.langis@gmail.com>
2024-09-09 07:30:18 -0400

    feat(analytics): SJIP-954 add analytics template

---
+++ packages/ui/src/components/Charts/ScatterPlot/type.ts
@@ -0,0 +1,31 @@
+import { ScatterPlotDatum, ScatterPlotLayerProps, ScatterPlotNodeData } from '@nivo/scatterplot';
+
+export { ScatterPlotDatum, ScatterPlotLayerProps, ScatterPlotNodeData, ScatterPlotRawSerie } from '@nivo/scatterplot';
+
+export type TNodesList = ScatterPlotNodeData<ScatterPlotDatum>[];
+
+export type TRegion = {
+    range: {
+        x: number;
+        y: number;
+    };
+    center: {
+        x: number;
+        y: number;
+    };
+};
+
+export interface IScatterPlotSelectBoxLayer extends ScatterPlotLayerProps<ScatterPlotDatum> {
+    active?: boolean;
+    handleOnSelect: (nodes: TNodesList) => void;
+    selectedNodes: TNodesList;
+    animate: boolean;
+    setAnimate: (value: boolean) => void;
+}
+
+export interface IScatterPlotSelectBoxCanvasLayer
+    extends Omit<ScatterPlotLayerProps<ScatterPlotDatum>, 'xScale' | 'yScale'> {
+    active?: boolean;
+    handleOnSelect: (nodes: TNodesList) => void;
+    selectedNodes: TNodesList;
+}
