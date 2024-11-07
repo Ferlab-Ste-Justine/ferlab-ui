5d7cde924cc161877203dc41a9227be4f566f28d
Olivier CP <28421005+oliviercperrier@users.noreply.github.com>
2024-11-07 10:48:20 -0500

    feat: improve pro label (#545)

--- packages/ui/src/components/ProLabel/index.tsx
+++ packages/ui/src/components/ProLabel/index.tsx
@@ -1,18 +1,27 @@
 import React, { ReactElement } from 'react';
 import { InfoCircleOutlined } from '@ant-design/icons';
-import { Popover, PopoverProps, Typography } from 'antd';
+import { Popover, PopoverProps, Tooltip, TooltipProps, Typography } from 'antd';
 import cx from 'classnames';
 
 import styles from './index.module.css';
 
-export interface IProLabelProps {
+interface IProLabelWithPopover {
+    popoverProps?: PopoverProps;
+    tooltipProps?: never;
+}
+
+interface IProLabelWithTooltip {
+    popoverProps?: never;
+    tooltipProps?: TooltipProps;
+}
+
+export type IProLabelProps = {
     title: string;
     className?: string;
-    popoverProps?: PopoverProps;
     colon?: boolean;
     size?: 'small' | 'default';
     requiredMark?: boolean;
-}
+} & (IProLabelWithPopover | IProLabelWithTooltip);
 
 const { Text } = Typography;
 
@@ -23,15 +32,22 @@ const ProLabel = ({
     requiredMark = false,
     size = 'default',
     title,
+    tooltipProps,
 }: IProLabelProps): ReactElement => (
     <Text className={cx(styles.fuiProLabel, requiredMark ? styles.requiredMark : '', className)}>
         <span>
             <span className={cx(styles.title, styles[size])}>{title}</span>
-            {popoverProps && (
+            {(popoverProps || tooltipProps) && (
                 <Text className={styles.infoIconWrapper} type="secondary">
-                    <Popover {...popoverProps}>
-                        <InfoCircleOutlined />
-                    </Popover>
+                    {tooltipProps ? (
+                        <Tooltip {...tooltipProps}>
+                            <InfoCircleOutlined />
+                        </Tooltip>
+                    ) : (
+                        <Popover {...popoverProps}>
+                            <InfoCircleOutlined />
+                        </Popover>
+                    )}
                 </Text>
             )}
             {colon && <span className={styles.colon}>:</span>}
