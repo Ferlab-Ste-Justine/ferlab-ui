72565c5254c57d63523d075619733f2c53bf8487
Gaelle <133775440+AltefrohneGaelle@users.noreply.github.com>
2024-12-04 10:50:02 -0500

    feat(landing): SJIP-1103 add view all studies btn (#555)

--- packages/ui/src/pages/LandingPage/Studies/index.tsx
+++ packages/ui/src/pages/LandingPage/Studies/index.tsx
@@ -6,28 +6,36 @@ import Summary from './Summary';
 import styles from './index.module.css';
 
 export type TStudiesDictionary = {
-    title: string;
     summary: string;
+    title: string;
+    viewAllBtn?: string;
 };
 
 export type TStudy = {
     code: string;
-    title: ReactNode;
-    subtitle: ReactNode;
     description: ReactNode;
     participantCount?: number;
+    subtitle: ReactNode;
+    title: ReactNode;
 };
 
 export type TStudiesProps = {
-    studiesCount: number;
-    studies: TStudy[];
     autoplaySpeed?: number;
     dictionary: TStudiesDictionary;
+    studies: TStudy[];
+    studiesBtnOnClick?: () => void;
+    studiesCount: number;
 };
 
-const Studies = ({ autoplaySpeed, dictionary, studies, studiesCount }: TStudiesProps): ReactElement => (
+const Studies = ({
+    autoplaySpeed,
+    dictionary,
+    studies,
+    studiesBtnOnClick,
+    studiesCount,
+}: TStudiesProps): ReactElement => (
     <div className={styles.container}>
-        <Summary dictionary={dictionary} studiesCount={studiesCount} />
+        <Summary dictionary={dictionary} studiesBtnOnClick={studiesBtnOnClick} studiesCount={studiesCount} />
         <Carousel autoplaySpeed={autoplaySpeed} studies={studies} />
     </div>
 );
