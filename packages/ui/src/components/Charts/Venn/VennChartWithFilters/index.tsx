d003377785b064f5831dd94415799dc8d2a2c47d
GaelleA <galtefrohne@ferlab.bio>
2025-04-07 09:43:59 -0400

    feat(venn): SJIP-1254 clean

--- packages/ui/src/components/Charts/Venn/VennChartWithFilters/index.tsx
+++ packages/ui/src/components/Charts/Venn/VennChartWithFilters/index.tsx
@@ -81,7 +81,7 @@ const VennChartWithFilters = ({
     const [selectedSets, setSelectedSets] = useState<ISetOperation[]>([]);
     const [entityCompared, setEntityCompared] = useState<string>(entitySelected);
     const [setIdsCompared, setSetIdsCompared] = useState<string[]>(idsSelected);
-    console.log('selected ids', idsSelected);
+
     if (loading) {
         return <VennWithFilterSkeleton />;
     }
